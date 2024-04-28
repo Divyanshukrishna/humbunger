01e36094386f8cd828f880e21bbdafccf85d4d24
Divyanshu <divyanshukrishna11@gmail.com>
2024-04-28 22:40:04 +0530

    humburger

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menuItems = document.getElementById("menuItems");
+    if (menuItems.style.display === "flex") {
+      menuItems.style.display = "none";
+    } else {
+      menuItems.style.display = "flex";
+    }
+  }
+  
